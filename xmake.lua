81b22cef1d3b3294ad09cd99ca510a1b6356d9dc
Ebrahim <ebrahim@gnu.org>
2026-05-07 00:34:23 +0330

    Minor

--- xmake.lua
+++ xmake.lua
@@ -6,10 +6,10 @@ target("showin", function()
     add_syslinks("user32", "gdi32", "advapi32", "kernel32")
     set_exceptions("none")
     if is_plat("mingw") then
-        add_ldflags("-mwindows", "-estart", "-nostartfiles", {force = true})
         add_cxxflags("-fno-rtti", "-fno-threadsafe-statics", {force = true})
+        add_ldflags("-mwindows", "-estart", "-nostartfiles", {force = true})
     elseif is_plat("windows") then
-        add_ldflags("/SUBSYSTEM:WINDOWS", "/entry:start", "/MERGE:.rdata=.text", {force = true})
-        add_cxxflags("/GR-", "/GS-", "/Zc:threadSafeInit-", {force = true})
+        add_cxxflags("/GR-", "/GS-", "/O1", "/Zc:threadSafeInit-", {force = true})
+        add_ldflags("/SUBSYSTEM:WINDOWS,5.01", "/INCREMENTAL:NO", "/entry:start", "/MERGE:.rdata=.text", {force = true})
     end
 end)
